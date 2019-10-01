e2447e868f44f21a391a8da4b2e8eb3e38d8ab26
Spencer Perkins <hello@spencerperkins.me>
2019-10-01 00:13:59 -0700

    Added map box, formatted map, and added a pin

--- scripts.js
+++ scripts.js
@@ -140,5 +140,13 @@ $(window).resize(function() {
   } 
 });
 
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3Blbm55cDMiLCJhIjoiY2pnb2I2azZ4MjI3ZjJ4cW4ycGR0djN6NiJ9.ZMKwiY9LeX2fWuWQ9fEWUw';
+var map = new mapboxgl.Map({
+  container: 'testId',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+
 
 
