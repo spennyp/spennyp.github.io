9ecb1b66446ad1085f1da7481861e745e99c1d8b
Spencer Perkins <hello@spencerperkins.me>
2020-04-02 12:54:44 -0700

    Autoselected search bar, fixed opacity issue

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -31,6 +31,7 @@ function formatTime(i) {
 }
 
 async function refreshPage() {
+    document.getElementById("searchText").select(); // Make the cursor select the search box on load
     setName();
     setTimeOfDay();
     updateBackgroundImg();
