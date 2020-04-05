fa71aa58a052cd26bb596be85e5d3d8eb2cf30c4
Spencer Perkins <hello@spencerperkins.me>
2020-04-05 03:30:51 -0700

    Fixed sim sizing bug

--- pendulumSimulation/pendulum.js
+++ pendulumSimulation/pendulum.js
@@ -20,8 +20,8 @@ var phiDot2IC = 4 * granularity;
 var canvasWidth = window.innerWidth; // may want to set based on screen size
 var canvasHeight = window.innerHeight;
 var strokeColor = "rgb(168, 35, 48)";
-var l1 = window.innerHeight / 4 - 20;
-var l2 = window.innerHeight / 4 - 20;
+var l1 = Math.min(window.innerHeight / 4, window.innerWidth / 4)
+var l2 = Math.min(window.innerHeight / 4, window.innerWidth / 4)
 
 var theta1 = [];
 var phi1 = [];
