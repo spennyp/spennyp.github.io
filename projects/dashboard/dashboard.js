d7b4680f00502770cf4d05fff66dadd6a55be887
Spencer <hello@spencerperkins.me>
2021-04-15 03:26:59 -0700

    Added defualt to gray on upsplash error for dashboard

--- projects/dashboard/dashboard.js
+++ projects/dashboard/dashboard.js
@@ -83,8 +83,8 @@ function updateBackgroundImg() {
     const collections = ""
 
     // Three different accounts for testing (keep running out of requests)
-    //var clientId = "c_Mh-IYia-rRAxpMVjEXew6rcrvySdE3lVI3xnG4Vrk";
-    var clientId = "9i_xH3I1PGmz4bSd5TjXVViHg7hq2WUz9iVEyq74POA";
+    var clientId = "c_Mh-IYia-rRAxpMVjEXew6rcrvySdE3lVI3xnG4Vrk";
+    //var clientId = "9i_xH3I1PGmz4bSd5TjXVViHg7hq2WUz9iVEyq74POA";
     // var clientId = "0K6ecqqXYKMvm7wJt8GpZ0qA6tpvbkqjNrQ8e6qKpP0";
 
     var url = "https://api.unsplash.com/photos/random/?client_id=" + clientId + "&orientation=landscape&collections=" + collections;
@@ -98,6 +98,7 @@ function updateBackgroundImg() {
         })
         .catch(() => {
             console.log("Error");
+			document.body.style.backgroundColor = "#474749"; // Set to gray if there is an error
         });
 }
 
